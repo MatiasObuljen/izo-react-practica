4184333efcda1bd90bb4e8d8a88605bebc217947
MatiasObuljen <matiasobuljen@gmail.com>
2023-10-20 00:12:05 -0300

    Práctica 4 - Tarea 3 (switch) completado

--- src/practica_4/Practica4.js
+++ src/practica_4/Practica4.js
@@ -14,31 +14,29 @@ export default function Practica4() {
     };
 
     const onChangeCheckbox = (event) => event.target.checked ? setcheckbox(1) : setcheckbox(0);
-    const onChangeGate = (event) => {
-        setGate(event.target.value);
-        //console.log(result);
-    };
+    const onChangeGate = (event) => setGate(event.target.value);
 
     const onChangeResult = () => {
+        const t = '( true ) .... ', f = '( false ) .... '
         switch (gate) {
             case 'AND':
-                input1 && checkbox ? setResult('true') : setResult('false');
+                input1 && checkbox ? setResult(t + gate) : setResult(f + gate);
                 break;
             case 'OR':
-                input1 || checkbox ? setResult('true') : setResult('false');
+                input1 || checkbox ? setResult(t + gate) : setResult(f + gate);
                 break;
             case 'NAND':
-                input1 && checkbox ? setResult('true') : setResult('false');
+                (!(input1 && checkbox)) ? setResult(t + gate) : setResult(f + gate);
                 break;
             case 'NOR':
-                (!(input1 || checkbox)) ? setResult('true') : setResult('false');
+                (!(input1 || checkbox)) ? setResult(t + gate) : setResult(f + gate);
                 break;
             case 'XOR':
-                input1 ^ checkbox ? setResult('true') : setResult('false');
+                input1 ^ checkbox ? setResult(t + gate) : setResult(f + gate);
                 break;
 
             default:
-                setResult('true');
+                setResult('default');
                 break;
         }
     };
@@ -46,36 +44,35 @@ export default function Practica4() {
     return (
         <div className="App">
             <div>
-                <label htmlFor="input1">Entrada 1:</label>
+                <label htmlFor="input1"></label><br/>
                 <input
                     id="input1"
                     type="number"
                     value={input1}
                     onChange={onChangeInput}
-                />
-                <label htmlFor="input1"> ---- Resultado: {input1 === 0 ? 'false' : 'true'} </label>
+                /><br/>
+                <label htmlFor="input1"> ___________________________________ Resultado 1: { input1 === 0 ? '( false )' : '( true )' } </label><br/><br/>
             </div>
             <div>
                 <label>
-                    <input
-                        type="checkbox"
-                        onChange={onChangeCheckbox}
-
-                    /> Entrada 2
-                </label>
-                <label htmlFor="input1"> ---------------------------------- Resultado: {checkbox === 0 ? 'false' : 'true'} </label>
+                    <input type="checkbox" onChange={onChangeCheckbox}/>
+                </label><br/>
+                <label htmlFor="input1"> ___________________________________ Resultado 2: { checkbox === 0 ? '( false )' : '( true )' }</label><br/><br/>
             </div>
             <div>
-                <label htmlFor="selectGate">Compuerta lógica</label>
-                <select id="selectGate" onChange={(event, gate) => { onChangeGate(event); onChangeResult(gate); }}>
+                <label htmlFor="selectGate">Compuerta lógica </label>
+                <select id="selectGate" onChange={ (event) => onChangeGate(event) }>
                     {logicGatesArray.map((element, index) => {
                         return <option key={index} value={element}>{element}</option>;
                     }
                     )}
-                </select>
-                <label htmlFor="sarasa"> ----------------- Resultado: {gate} </label>
+                </select><br/><br/>
+                <label> ___________________________________ Compuerta: {gate} </label><br/><br/>
             </div>
-            <div><span>--------------------------------------------------- Salida: {result}</span></div>
+            <br/>
+            <button onClick={onChangeResult}> CALCULAR COMPUERTA </button>
+            <br/><br/>
+            <div><span> ___________________________________ Resultado: {result}</span></div>
         </div >
     );
 };
